b842d7743c9b40bef3171145b050e9bc366c7001
donbasta <faddilafarras@gmail.com>
2021-09-26 19:21:54 +0700

    feat: enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express';
 import axios from 'axios';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
